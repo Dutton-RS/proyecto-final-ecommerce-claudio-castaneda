e4a190a2380da47854f7e9f1178660edae097848
Claudio Castañeda <claudiocastaneda@Mac.fibertel.com.ar>
2025-06-30 19:53:40 -0300

    Proyecto final V.2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,43 +1,5 @@
 
-// authController.js
 
-// import { generateToken } from '../utils/token-generator.js';
-// import Usuario from '../models/Usuario.js'; 
-// import bcrypt from 'bcryptjs'; 
-
-// export async function login(req, res) {
-//   const { email, password } = req.body;
-
-//   try {
-    
-//     const userFromDB = await Usuario.getByEmail(email); 
-
-//     if (!userFromDB) {
-//       return res.status(401).json({ message: "Invalid email or password" });
-//     }
-
-//     const passwordMatch = await bcrypt.compare(password, userFromDB.password);
-
-//     if (!passwordMatch) {
-//       return res.status(401).json({ message: "Invalid email or password" });
-//     }
-
-    
-//     const token = generateToken({ id: userFromDB.id, email: userFromDB.email });
-
-//     res.status(200).json({
-//       message: "Login successful",
-//       token: token,
-//       user: { id: userFromDB.id, email: userFromDB.email } 
-//     });
-
-//   } catch (error) {
-//     console.error("Error during login:", error);
-//     res.status(500).json({ message: "Internal server error during login" });
-//   }
-// }
-
-//------------------------------------------------------------------------------------
 
 // src/controllers/authController.js
 
